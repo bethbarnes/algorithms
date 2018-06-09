d4f7c244f2f10cc8ba8dc42ee58f573da07ed5b5
Beth Barnes <bethcbarnes@outlook.com>
2018-06-09 11:34:19 -0400

    initial commit

---
+++ problems/tree-traversal.js
@@ -0,0 +1,9 @@
+// Interviewer Prompt
+// Today you will write a series of iterator functions for trees.
+
+// breadthFirst
+// depthFirstPreOrder
+// (if time allows)
+
+// depthFirstPostOrder
+// Each of these function will take a node of the tree and a callback. The function will iterate through the child nodes, calling the callback function on each of them. The difference between them is the order in which they iterate.
